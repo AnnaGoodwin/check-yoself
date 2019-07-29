d1640efed72422afb0bcf49ff6fd66f4bc5b9f0d
Anna Goodwin <goodwin.musician@gmail.com>
2019-07-29 10:15:42 -0600

    Fix to do class

---
+++ todo-list.js
@@ -0,0 +1,28 @@
+class ToDoList {
+
+  constructor(id, title, urgent, tasks) {
+    this.id = id;
+    this.title = title;
+    this.urgent = urgent || false;
+    this.tasks = tasks || [];
+  }
+
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateToDo() {
+  
+  }
+
+  updateTask() {
+
+  }
+
+  addTask() {
+
+}
